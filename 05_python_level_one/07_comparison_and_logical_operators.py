aeb0e6f38955badc41ed938f1dbb2ded77f2c7ee
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-03-03 22:55:37 -0300

    comparison and logical operators

---
+++ 05_python_level_one/07_comparison_and_logical_operators.py
@@ -0,0 +1,27 @@
+#### COMPARISON OPERATORS ####
+
+# Greater than
+print(1 > 2)
+# Less than
+print(1 < 2)
+# Greater than or Equal to
+print(1 >= 1)
+# Less than or Equal to
+print(1 <= 4)
+# Equality
+print(1 == 1)
+print(1 == "1")
+print('hi' == 'bye')
+# Inequality
+print(1 != 2)
+
+#### LOGICAL OPERATORS ####
+
+# AND
+print((1 > 2) and (2 < 3))
+
+# OR
+print((1 > 2) or (2 < 3))
+
+# Multiple logical operators
+print((1 == 2) or (2 == 3) or (4 == 4))

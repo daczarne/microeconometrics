23d2434fa338699222dfd4a3ffeea4a4228b0d41
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-03-03 22:37:21 -0300

    numbers

---
+++ 05_python_level_one/01_numbers.py
@@ -0,0 +1,37 @@
+## Math
+
+# Addition
+2 + 1
+
+# Subtraction
+2 - 1
+
+# Multiplication
+2 * 2
+
+# Division
+3 / 2
+
+# Powers
+2 ** 3
+4 ** 0.5
+
+# Order of Operations followed in Python
+2 + 10 * 10 + 3
+
+# Can use parenthesis to specify orders
+(2+10) * (10+3)
+
+## Variables
+
+a = 5
+print(a + a)
+a = 10
+print(a)
+a += a
+print(a)
+
+puppies = 6
+weight = 2
+total_weight = puppies * weight
+print(total_weight)

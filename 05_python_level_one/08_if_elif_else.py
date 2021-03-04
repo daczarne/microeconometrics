a80d1e8b00e0ed1d82406ce29085e9b7d56fd448
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-03-03 22:57:01 -0300

    if elif else

---
+++ 05_python_level_one/08_if_elif_else.py
@@ -0,0 +1,27 @@
+
+if 1 < 2:
+    print('Yep!')
+
+
+if 1 < 2:
+    print('yep!')
+
+
+if 1 < 2:
+    print('first')
+else:
+    print('last')
+
+
+if 1 > 2:
+    print('first')
+else:
+    print('last')
+
+
+if 1 == 2:
+    print('first')
+elif 3 == 3:
+    print('middle')
+else:
+    print('Last')
